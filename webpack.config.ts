213b76aa899c02ebd3094997ad7fd54543bbc544
Pete Savage <psavage@redhat.com>
2021-11-23 16:41:17 +0000

    Fixed

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,18 +1,30 @@
 /* eslint-env node */
 
 import * as webpack from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
 import * as path from 'path';
+import * as MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { ConsoleRemotePlugin } from '@openshift-console/dynamic-plugin-sdk/lib/index-webpack';
 
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const extractCSS = new MiniCssExtractPlugin();
+const overpassTest = /overpass-.*\.(woff2?|ttf|eot|otf)(\?.*$|$)/;
 
-const config: webpack.Configuration = {
+interface Configuration extends webpack.Configuration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
+const config: Configuration = {
   mode: 'development',
   context: path.resolve(__dirname, 'src'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name]-bundle.js',
     chunkFilename: '[name]-chunk.js',
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9001,
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
@@ -31,18 +43,64 @@ const config: webpack.Configuration = {
           },
         ],
       },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules\/(?!(@patternfly)\/).*/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: './',
+            },
+          },
+          { loader: 'cache-loader' },
+          { loader: 'thread-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'resolve-url-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+              outputStyle: 'compressed',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff2?|ttf|eot|otf)(\?.*$|$)/,
+        exclude: overpassTest,
+        loader: 'file-loader',
+        options: {
+          name: 'assets/[name].[ext]',
+        },
+      },
     ],
   },
   plugins: [
+    extractCSS,
     new ConsoleRemotePlugin(),
-    new CopyWebpackPlugin({
-      patterns: [{ from: path.resolve(__dirname, 'locales'), to: 'locales' }],
+    new webpack.EnvironmentPlugin({
+      REACT_APP_API_ROOT: '',
+      REACT_APP_BUILD_MODE: '',
     }),
   ],
   devtool: 'source-map',
   optimization: {
     chunkIds: 'named',
     minimize: false,
+  },
+  externals: {
+    '@openshift-console/dynamic-plugin-sdk/api': 'api',
   },
 };
 
