ee3c9521ddcc13a7cf6d01a133bd98803d37d658
Gabor Burges <gburges@redhat.com>
2025-05-30 16:57:27 +0200

    Add build deploy back

---
+++ build-deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -exv
+
+IMAGE="quay.io/cloudservices/clowder-plugin"
+IMAGE_TAG=$(git rev-parse --short=7 HEAD)
+SECURITY_COMPLIANCE_TAG="sc-$(date +%Y%m%d)-$(git rev-parse --short=7 HEAD)"
+
+if [[ -z "$QUAY_USER" || -z "$QUAY_TOKEN" ]]; then
+    echo "QUAY_USER and QUAY_TOKEN must be set"
+    exit 1
+fi
+
+if [[ -z "$RH_REGISTRY_USER" || -z "$RH_REGISTRY_TOKEN" ]]; then
+    echo "RH_REGISTRY_USER and RH_REGISTRY_TOKEN  must be set"
+    exit 1
+fi
+
+# If the "security-compliance" branch is used for the build, it will tag the image as such.
+if [[ "$GIT_BRANCH" == "origin/security-compliance" ]]; then
+    IMAGE_TAG="$SECURITY_COMPLIANCE_TAG"
+fi
+
+DOCKER_CONF="$PWD/.docker"
+mkdir -p "$DOCKER_CONF"
+docker --config="$DOCKER_CONF" login -u="$QUAY_USER" -p="$QUAY_TOKEN" quay.io
+docker --config="$DOCKER_CONF" login -u="$RH_REGISTRY_USER" -p="$RH_REGISTRY_TOKEN" registry.redhat.io
+docker --config="$DOCKER_CONF" build -t "${IMAGE}:${IMAGE_TAG}" .
+docker --config="$DOCKER_CONF" push "${IMAGE}:${IMAGE_TAG}"
